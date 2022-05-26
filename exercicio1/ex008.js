0127276ee75c93bf320d6fe8ff7810985eb719cb
Vanessa Ávila <vanessa.3bl@gmail.com>
2022-05-26 10:29:14 +0100

    Update

---
+++ exercicio1/ex008.js
@@ -0,0 +1,6 @@
+var vel = 15
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Limite de velocidade ultrapassado. Você foi Multado!')
+}
+    console.log('Conduza sempre com cinto de segurança!')
